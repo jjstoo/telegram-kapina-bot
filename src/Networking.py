f90f72468862984415be55d2c75c759ecee768ba
Joni Lepistö <joni.m.lepisto@gmail.com>
2020-07-02 19:18:29 +0300

    Rebuilt connectivity solutions to support proxying

--- src/Networking.py
+++ src/Networking.py
@@ -1,23 +1,59 @@
 import requests
+import random
 from typing import Dict
 
 
-def https_post(url: str, parameters: Dict = None, files: Dict = None) -> requests.Response:
-    """
-    Raw https post with optional parameters
-    :param url: POST URL
-    :param parameters: Parameters
-    :param files: Multipart file data to send
-    :return: None
-    """
-    return requests.post(url, parameters, files=files)
+class NetworkHandler:
+
+    def __init__(self):
+
+        self.proxies = None
+
+    def https_post(self, url: str, parameters: Dict = None, files: Dict = None) -> requests.Response:
+        """
+        Raw https post with optional parameters
+        :param url: POST URL
+        :param parameters: Parameters
+        :param files: Multipart file data to send
+        :return: None
+        """
+        return requests.post(url, parameters, files=files)
+
+    def https_get(self, url: str, parameters: Dict = None, headers: Dict = None) -> requests.Response:
+        """
+        Raw https get with optional parameters
+        :param url: GET URL
+        :param parameters: Parameters
+        :param headers: Header attributes
+        :return: Requests GET object with return code and payload
+        """
+        return requests.get(url, params=parameters, headers=headers, proxies=self.proxies)
+
+    def set_random_proxy(self):
+        proxy_list_url = "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt"
+        test_url = 'https://httpbin.org/ip'
+        data = requests.get(proxy_list_url).text.split("\n")
+        random.shuffle(data)
+
+        for ip in data:
+            print("Connecting to proxy at " + ip, end=" ... ")
+            proxies = {
+                "http": "http://" + ip,
+                "https": "https://" + ip
+            }
+
+            try:
+                response = requests.get(test_url, proxies=proxies, timeout=5).status_code
+                if response == 200:
+                    print("SUCCESS")
+                    self.proxies = proxies
+                    return
+            except Exception as e:
+                print("FAILED")
+
+        print("No working proxy found")
 
 
-def https_get(url: str, parameters: Dict = None, headers: Dict = None) -> requests.Response:
-    """
-    Raw https get with optional parameters
-    :param url: GET URL
-    :param parameters: Parameters
-    :return: Requests GET object with return code and payload
-    """
-    return requests.get(url, params=parameters, headers=headers)
+if __name__ == "__main__":
+    net = NetworkHandler()
+    net.set_random_proxy()
--- src/UntappdCrawler.py
+++ src/UntappdCrawler.py
@@ -2,7 +2,7 @@ from concurrent.futures import ThreadPoolExecutor
 from typing import Dict
 from bs4 import BeautifulSoup
 
-from Networking import https_get
+from Networking import NetworkHandler
 
 # We'll have to fake being a real user, otherwise Untappd blocks the requests
 common_header = {
@@ -30,12 +30,16 @@ class Beer:
         self.img = img
 
 
+
+
 class UntappdCrawler:
     BASE_URL = "https://untappd.com"
 
     def __init__(self):
         self.beer_lists = {}
         self.default_beer_list = None
+        self.net = NetworkHandler()
+        self.net.set_random_proxy()
 
     def set_beer_lists(self, lists: Dict):
         """
@@ -60,25 +64,30 @@ class UntappdCrawler:
     def get_beers_on_list(self, list: str = None):
 
         beers = []
-        raw_beer_list = []
         beer_futures = []
+        try:
+            data = self.net.https_get(self.beer_lists[list][0], headers=common_header).text
+            soup = BeautifulSoup(data, features="html5lib")
+            raw_beer_list = soup.find("ul", {"class": "menu-section-list"})
+            for beer in raw_beer_list.find_all("li"):
+                beer_futures.append(tpe.submit(self.get_beer, UntappdCrawler.BASE_URL + beer.find("a", href=True)["href"]))
 
-        data = https_get(self.beer_lists[list][0], headers=common_header).text
-        soup = BeautifulSoup(data)
-        print(soup)
-        raw_beer_list = soup.find(id=self.beer_lists[list][1])
-        for beer in raw_beer_list.find_all("li"):
-            beer_futures.append(tpe.submit(self.get_beer, UntappdCrawler.BASE_URL + beer.find("a", href=True)["href"]))
+            for future in beer_futures:
+                result = future.result()
+                if result is not None:
+                    beers.append(result)
 
-        for future in beer_futures:
-            result = future.result()
-            if result:
-                beers.append(result)
+            return beers
+
+        except Exception as e:
+            print("Connection to untappd failed, resetting proxy")
+            self.net.set_random_proxy()
+            return None
 
     def get_beer(self, url: str):
         try:
-            data = https_get(url, headers=common_header).text
-            soup = BeautifulSoup(data)
+            data = self.net.https_get(url, headers=common_header).text
+            soup = BeautifulSoup(data, features="html5lib")
 
             basic_info = soup.find("div", {"class": "name"})
             name = basic_info.find("h1").text
@@ -98,5 +107,20 @@ class UntappdCrawler:
                         abv=abv,
                         style=style,
                         img=img)
-        except Exception as e:
-            return None
+        except Exception:
+            raise ConnectionError
+
+
+if __name__ == "__main__":
+    untappd = UntappdCrawler()
+    untappd.set_beer_lists({"hana":
+        [
+            "https://untappd.com/v/pub-kultainen-apina/17995?ng_menu_id=5035026b-1470-48c7-b82a-bf1df18f5889",
+            "section-menu-list-146318694"]})
+
+    untappd.set_default_beer_list("hana")
+    beers = untappd.get_beers_on_list("hana")
+
+    for beer in beers:
+        print(beer.name)
+
