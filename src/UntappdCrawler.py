0d24e154f00d0ff752a0f0e216b0b119ab45b81a
Joni Lepistö <joni.m.lepisto@gmail.com>
2020-07-02 15:22:01 +0300

    Development halted due to untappd ban. Current state not tested but should be ok.

--- src/UntappdCrawler.py
+++ src/UntappdCrawler.py
@@ -15,13 +15,17 @@ tpe = ThreadPoolExecutor(max_workers=10)
 class Beer:
     def __init__(self,
                  name,
+                 brewery,
                  abv,
                  rating,
+                 ratings,
                  style,
                  img):
         self.name = name
+        self.brewery = brewery
         self.abv = abv
         self.rating = rating
+        self.ratings = ratings
         self.style = style
         self.img = img
 
@@ -55,18 +59,44 @@ class UntappdCrawler:
 
     def get_beers_on_list(self, list: str = None):
 
-        raw_beer_entries = []
+        beers = []
+        raw_beer_list = []
+        beer_futures = []
 
         data = https_get(self.beer_lists[list][0], headers=common_header).text
         soup = BeautifulSoup(data)
+        print(soup)
         raw_beer_list = soup.find(id=self.beer_lists[list][1])
         for beer in raw_beer_list.find_all("li"):
-            tpe.submit(self.get_beer, UntappdCrawler.BASE_URL + beer.find("a", href=True)["href"])
+            beer_futures.append(tpe.submit(self.get_beer, UntappdCrawler.BASE_URL + beer.find("a", href=True)["href"]))
+
+        for future in beer_futures:
+            result = future.result()
+            if result:
+                beers.append(result)
 
     def get_beer(self, url: str):
-        data = https_get(url, headers=common_header).text
-        soup = BeautifulSoup(data)
+        try:
+            data = https_get(url, headers=common_header).text
+            soup = BeautifulSoup(data)
 
-        basic_info = soup.find("div", {"class": "name"})
-        name = basic_info.find("h1").text
-        print(name)
+            basic_info = soup.find("div", {"class": "name"})
+            name = basic_info.find("h1").text
+            brewery = basic_info.find("p", {"class": "brewery"}).find("a").text
+            style = basic_info.find("p", {"class": "style"}).text
+            img = soup.find("a", {"class": "label"}).find("img")["src"]
+
+            details = soup.find("div", {"class": "details"})
+            abv = details.find("p", {"class": "abv"}).text
+            rating = details.find("div", {"class": "caps"})["data-rating"]
+            ratings = details.find("p", {"class": "raters"}).text
+
+            return Beer(name=name,
+                        brewery=brewery,
+                        rating=float(rating),
+                        ratings=ratings,
+                        abv=abv,
+                        style=style,
+                        img=img)
+        except Exception as e:
+            return None
